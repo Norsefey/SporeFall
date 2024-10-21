a3770b01969c0306292466246464c109fd32f2a0
ZombieCow323 <149716539+ZombieCow323@users.noreply.github.com>
2024-10-21 16:09:50 -0400

    TrainUIMinorChange

--- SporeFall/Assets/Train/Scripts/TrainUI.cs
+++ SporeFall/Assets/Train/Scripts/TrainUI.cs
@@ -7,11 +7,12 @@ using TMPro;
 public class TrainUI : MonoBehaviour
 {
     private TrainHandler tHandler;
-    public Slider HPBar;
+    [SerializeField] private Slider HPBar;
 
     void Start()
     {
         HPBar.maxValue = tHandler.maxHP;
+        Debug.Log("Train HP bar set to 100");
     }
 
     // Update is called once per frame
@@ -23,5 +24,6 @@ public class TrainUI : MonoBehaviour
     public void UpdateHPDisplay(float value)
     {
         HPBar.value = value;
+        Debug.Log("Updating train HP bar");
     }
 }
