be15d7db87cb3944919f3374a1cb2afe58ceb426
ZombieCow323 <averysmorris@gmail.com>
2025-03-27 17:02:26 -0400

    WinLoseController
    Controller works properly in win/lose scenes

--- SporeFall/Assets/Menus/Scripts/WinLoseMenus.cs
+++ SporeFall/Assets/Menus/Scripts/WinLoseMenus.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class WinLoseMenus : MonoBehaviour
 {
@@ -10,6 +12,23 @@ public class WinLoseMenus : MonoBehaviour
 
     //[SerializeField] string level1Name;
     [SerializeField] int gameSceneIndex;
+    [SerializeField] GameObject firstButton;
+
+    private void Start()
+    {
+        InputSystem.onDeviceChange += OnDeviceChange;
+        if (InputSystem.devices.Count > 2)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(firstButton);
+        }
+    }
+
+    private void OnDisable()
+    {
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.F))
@@ -27,6 +46,8 @@ public class WinLoseMenus : MonoBehaviour
             }
         }
     }
+
+
     public void RetryLevel()
     {
         SceneManager.LoadScene(gameSceneIndex);
@@ -40,5 +61,20 @@ public class WinLoseMenus : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        if (change == InputDeviceChange.Added)
+        {
+            Debug.Log($"Device Connected: {device.displayName}");
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(firstButton);
+        }
+        else if (change == InputDeviceChange.Disconnected)
+        {
+            Debug.Log($"Device Disconnected: {device.displayName}");
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 }
